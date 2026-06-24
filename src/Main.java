42f9265c34e6d41895273e4379219866b00f1c3d
Nicolas <adilsonq1976@gmail.com>
2026-06-24 15:48:37 -0300

    Criando Classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Catalogo catalogo = new Catalogo();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        // Carros
+        catalogo.adicionar(new Carro("Toyota", "Corolla", 2022, 120000.0, 4, "Flex"));
+        catalogo.adicionar(new Carro("Fiat", "Pulse", 2023, 98000.0, 4, "Flex"));
+        catalogo.adicionar(new Carro("Volkswagen", "Polo", 2023, 105000.0, 4, "Flex"));
+        catalogo.adicionar(new Carro("Chevrolet", "Onix", 2022, 89000.0, 4, "Flex"));
+        catalogo.adicionar(new Carro("Hyundai", "HB20", 2021, 78000.0, 4, "Flex"));
+
+        // Motos
+        catalogo.adicionar(new Moto("Honda", "CB 500", 2021, 35000.0, false, "Esportiva"));
+        catalogo.adicionar(new Moto("Yamaha", "MT-07", 2022, 52000.0, false, "Naked"));
+        catalogo.adicionar(new Moto("Honda", "Biz 125", 2023, 14000.0, false, "Scooter"));
+
+        // Caminhões
+        catalogo.adicionar(new Caminhao("Volvo", "FH 540", 2020, 650000.0, 28.0, 6));
+        catalogo.adicionar(new Caminhao("Scania", "R 450", 2021, 580000.0, 25.0, 6));
+
+        System.out.println("=== Todos os veiculos ===");
+        catalogo.listarTodos();
+
+        System.out.println("\n=== Busca por marca: Fiat ===");
+        for (Veiculo v : catalogo.buscarPorMarca("Fiat")) {
+            v.exibirDetalhes();
+        }
+
+        System.out.println("\n=== Busca por marca: Honda ===");
+        for (Veiculo v : catalogo.buscarPorMarca("Honda")) {
+            v.exibirDetalhes();
+        }
+
+        System.out.println("\n=== Busca por marca: Volvo ===");
+        for (Veiculo v : catalogo.buscarPorMarca("Volvo")) {
+            v.exibirDetalhes();
+        }
+    }
 }
